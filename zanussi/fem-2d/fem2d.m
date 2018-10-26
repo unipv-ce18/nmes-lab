4ede4da55e0f55ecefa4dcc92d7c2d723d8ce39f
Luca Zanussi <luca.z@outlook.com>
2018-10-26 15:24:42 +0200

    +10x performance increase in 2D FEM

--- zanussi/fem-2d/fem2d.m
+++ zanussi/fem-2d/fem2d.m
@@ -2,11 +2,12 @@ oldpath = addpath('./vendor', './vendor/distmesh');
 
 % We need Symbolic Math to compute the exact laplacian for reference
 syms x y;
-Ugen(x, y) = sin(4*pi*sqrt(x^2+y^2))/sqrt(x^2+y^2);
-Fgen = -laplacian(Ugen, [x y]);
+UgenSym(x, y) = sin(4*pi*sqrt(x^2+y^2))/sqrt(x^2+y^2);
+Ugen = matlabFunction(UgenSym);
+Fgen = matlabFunction(-laplacian(UgenSym, [x y]));
 
 % Generate the mesh (and close that popup)
-[p, t, I, B] = triangular_mesh('circle', .1);
+[p, t, I, B] = triangular_mesh('circle', .05);
 close(1);
 
 % Allocate A and F and initialize to zero
@@ -15,27 +16,33 @@ A = spalloc(length(I), length(I), 7*length(I));
 F = zeros(length(I), 1);
 
 bh = waitbar(0, ...
-    sprintf('Evaluating on mesh... 0.00% (%d/%d polys)', 0, 0, length(t)), ...
+    sprintf('Evaluating on mesh... 0.00% (%d/%d polys)', 0, length(t)), ...
     'WindowStyle', 'modal');
 
 % Iterate over all the triangles
 for tIdx = 1:length(t)
-    
+
     % Get the indexes of the vertices of this triangle
     tDef = t(tIdx, :);
-    
+
     % And then get their coordinates
     tCoords = p(tDef, :);
-    
+
     if ispolycws(tCoords)
        warning('Encountered a clockwise-defined triangle'); 
     end
-    
+
     % Triangle area (http://www.mathopenref.com/coordtrianglearea.html)
     area = ( tCoords(1,1)*(tCoords(2,2)-tCoords(3,2)) + ...
              tCoords(2,1)*(tCoords(3,2)-tCoords(1,2)) + ...
              tCoords(3,1)*(tCoords(1,2)-tCoords(2,2)) ) / 2;
-    
+
+    % Since we are here, calculate also F contribution for this triangle,
+    % the value is const. for each vertex since the barycenter is the same.
+    %   Find the barycenter > Evaluate Fgen in it > Integrate (mul. area)
+    bary = sum(tCoords) / 3;
+    fVal = 1/3 * double(Fgen(bary(1), bary(2))) * area;
+
     % We need to iterate only on the vertices *inside* the mesh
     innerVerts = intersect(tDef, I, 'stable');
 
@@ -45,33 +52,33 @@ for tIdx = 1:length(t)
         iIdx = find(tDef == i);
         iCoordsSort = circshift(tCoords, -iIdx+1);
         ei = diff(iCoordsSort(2:end,:));
-        
+
         for j = innerVerts
             jIdx = find(tDef == j);
             jCoordsSort = circshift(tCoords, -jIdx+1);
             ej = diff(jCoordsSort(2:end,:));
-            
+
             A(i,j) = A(i,j) + dot(ei, ej)/(4*area);
         end
-        
-        % -- Generate also F for this triangle --
-        % We need to find the barycenter, evaluate F in it, then integrate
-        c = sum(tCoords) / 3;
-        F(i) = F(i) + 1/3 * double(Fgen(c(1), c(2))) * area;
+
+        % Apply also F contribution to this vertex
+        F(i) = F(i) + fVal;
     end
 
-    perc = tIdx/length(t);
-    waitbar(perc, bh, ...
-        sprintf('Evaluating on mesh... %.2f%% (%d/%d polys)', 100*perc, tIdx, length(t)));
+    if ~mod(tIdx, 200)
+        perc = tIdx/length(t);
+        waitbar(perc, bh, ...
+            sprintf('Evaluating on mesh... %.2f%% (%d/%d polys)', ...
+            100*perc, tIdx, length(t)));
+    end
 end
 
 U = A \ F;
-close(bh);
 
-%% -- Plot something --
-bh = waitbar(0, 'Initializing plot', 'WindowStyle', 'modal');
 
-% Configure
+
+% -- Start plotting --
+bh = waitbar(0, bh, 'Initializing plot', 'WindowStyle', 'modal');
 figure('Name', '2D FEM Plot');
 uistack(bh,'top');
 hold on
@@ -93,7 +100,7 @@ plotPtsY = plotPts(:,2);
 
 % Plot reference U on these points
 waitbar(.6, bh, 'Plotting target mesh points');
-Uref = arrayfun(@(x,y) double(Ugen(x,y)), plotPtsX, plotPtsY);
+Uref = arrayfun(@(x,y) Ugen(x,y), plotPtsX, plotPtsY);
 plot3(plotPtsX, plotPtsY, Uref, 'ob', 'LineWidth', 1);
 
 % Plot computed U on these points
