e668e2a1ce6f89662c7a1e79ed6d5d4ede2e09eb
Luca Zanussi <luca.z@outlook.com>
2018-11-04 22:53:45 +0100

    Finish B matrix section, add 2 new colors

--- assets/script/main.js
+++ assets/script/main.js
@@ -11,7 +11,9 @@ function onAutoLoad() {
         '\\greenhl': '\\textcolor{#{{ site.text_colors.green }}}{#1}',
         '\\bluehl': '\\textcolor{#{{ site.text_colors.blue }}}{#1}',
         '\\redhl': '\\textcolor{#{{ site.text_colors.red }}}{#1}',
-        '\\dredhl': '\\textcolor{#{{ site.text_colors.darkred }}}{#1}'
+        '\\dredhl': '\\textcolor{#{{ site.text_colors.darkred }}}{#1}',
+        '\\purplehl': '\\textcolor{#{{ site.text_colors.purple }}}{#1}',
+        '\\tealhl': '\\textcolor{#{{ site.text_colors.teal }}}{#1}'
       }
   });
 }
